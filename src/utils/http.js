56ab5e61eb6b5e4a4fc22846768042efe71e79e7
WatermelonFight <1022683550@qq.com>
2024-04-22 22:16:04 +0800

    https2

--- src/utils/http.js
+++ src/utils/http.js
@@ -8,7 +8,7 @@ import router from "@/router";
 
 //创建实例
 const httpInstance = axios.create({
-    baseURL:'https://pcapi-xiaotuxian-front-devtest.itheima.net',
+    baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout:5000
 })
 
